d7e10d358c1589e60907157b5216107d749d890d
AmoraMas <amoramas@protonmail.com>
2023-02-09 17:36:15 -0700

    Completed Features

--- index.js
+++ index.js
@@ -13,17 +13,15 @@ const buttonUpdate = document.getElementById('update-location');
 
 // Event Listeners for buttons
 buttonUpdate.addEventListener('click', updateLocationInfo);
-//buttonHideLists.addEventListener('click', emptyTable);
-
-// Find page areas
-//const dataArea = document.getElementById('dataArea');
 
 // Find table areas
 const $mealsList = $('.meals');
+const $ingredients = $('.ingredients');
 
 // Additional Global Variables
 let allItems = [];
 let allRecipies = [];
+let allMaterialGroups = [];
 
 // 
 //  FUNCTIONS
@@ -35,11 +33,13 @@ function updateLocationInfo() {
     emptyTable();
     $.get(`https://api.arsha.io/util/db/dump?lang=${language}`, (data) => { allItems = data; getAllMeals(); });
     $.get(`https://api.arsha.io/util/db/recipes/dump?lang=${language}`, (data) => { allRecipies = data; });
+    $.get(`https://api.arsha.io/util/db/recipes/matgroups/dump?lang=${language}`, (data) => { allMaterialGroups = data; });
 }
 
 
 function emptyTable() {
     $mealsList.empty();
+    $ingredients.empty();
 }
 
 
@@ -52,23 +52,22 @@ function getAllMeals() {
             object['name'] = allItems[i].name;
             object['id'] = allItems[i].id;
             object['icon'] = allItems[i].icon;
-            // console.log('Item: ', allItems.responseJSON[i].name, 'ID: ', allItems.responseJSON[i].id, 'Icon: ', allItems.responseJSON[i].icon);
             array.push(object);
         }
     }
     //console.log(array);
-    drawTable(array);
+    showMain(array);
 }
 
 
-function drawTable(arrayOfObjects) {
-    let $tableHeader = $('<div></div>').addClass('tableHeader').text('Meals you can Craft:');
+function showMain(arrayOfObjects) {
+    let $tableHeader = $('<div></div>').addClass('tableHeader').text('Meals you can Craft');
     $mealsList.append($tableHeader);
 
     let $table = $('<table></table>').addClass('table');
 
     let $thead = $('<thead></thead>').addClass('thead');
-    let headers = ['Item', 'Min Enhance', 'Max Enhance', 'Base Price', 'Amount Listed', 'Total Trades', 'Min Price', 'Max Price', 'Last Sold Price', 'Last Sold Time'];
+    let headers = ['Item', 'Min Enhance', 'Max Enhance', 'Base Price', '# Listed', 'Total Trades', 'Min Price', 'Max Price', 'Last Sold Price', 'Last Sold Time'];
     let $tr = $('<tr></tr>');
     for (let i = 0; i < headers.length; i++) {
         let $th = $('<th></th>').text(headers[i]);
@@ -84,6 +83,10 @@ function drawTable(arrayOfObjects) {
             //console.log('Msg: ', data.resultMsg);
             if (data.resultMsg != '0') {
                 let itemName = arrayOfObjects[i].name;
+                //console.log('Item: ', itemName);
+                if (itemName.includes(`&#39;`)) {
+                    itemName = itemName.replace('&#39;', '\'')
+                }
                 let simplerData = (data.resultMsg).split('-');
                 let $tr = $('<tr></tr>');
                 //console.log(simplerData);
@@ -93,7 +96,9 @@ function drawTable(arrayOfObjects) {
                     if (i == 0) {
                         $td.text(itemName).addClass('link');
                         $td.click((data) => {
+                            $ingredients.empty();
                             showIngredients(itemName);
+                            $tr.addClass('selected').siblings().removeClass('selected');
                         });
                     }
                     else {
@@ -112,13 +117,13 @@ function drawTable(arrayOfObjects) {
 
 
 function showIngredients(itemName) {
-    //console.log(data);
     //console.log(itemName);
+    let cantFindIt = true;
     console.log('Getting Ingredient List: ', itemName);
-    let array = [];
     for (let i = 0; i < allRecipies.length; i++) {
         if (allRecipies[i].name == itemName) {
             console.log(allRecipies[i]);
+            let id = allRecipies[i].id;
 
             let $container = $('<div></div>').addClass('ingredient-list');
 
@@ -128,7 +133,7 @@ function showIngredients(itemName) {
 
             let $table = $('<table></table>').addClass('table');
 
-            let headers = ['Quantity', ['Name'], ['Price'], ['Amount Listed']];
+            let headers = ['Quantity', 'Name', 'Price', '# Listed'];
             let $thead = $('<thead></thead>').addClass('thead')
             let $trhead = $('<tr></tr>');
             for (let i = 0; i < headers.length; i++) {
@@ -139,39 +144,135 @@ function showIngredients(itemName) {
             $table.append($thead);
             let $tbody = $('<tbody></tbody>').addClass('tbody');
             for (let j = 0; j < allRecipies[i].components.length; j++) {
+                let $name;
+                let $price;
+                let $numListed;
                 console.log('Getting Ingredient: ', allRecipies[i].components[j].id);
-                $('<td></td>').text($.get(`https://api.arsha.io/v1/${region}/item?id=${allRecipies[i].components[j].id}`, (data) => {
-                    let $name;
+                if ('materialGroup' in allRecipies[i].components[j]) {
                     let $tr = $('<tr></tr>');
                     let $quantity = $('<td></td>').text(allRecipies[i].components[j].quantity);
-                    for (let k = 0; k < allItems.length; k++) {
-                        if (allItems[k].id == allRecipies[i].components[j].id) {
-                            $name = $('<td></td>').text(allItems[k].name).addClass('link');
-                            $name.click(function () { showIngredients(allItems[k].name) });
-                            break;
-                        }
-                    }
-                    let $price = $('<td></td>').text((data.resultMsg).split('-')[8]);
-                    let $numListed = $('<td></td>').text((data.resultMsg).split('-')[4]);
+                    $name = $('<td></td>').text(`Material Group ${allRecipies[i].components[j].materialGroup}`).addClass('link');
+                    $name.click(function () { showMaterialGroup(allRecipies[i].components[j].materialGroup) });
+                    $price = $('<td></td>').text('---');
+                    $numListed = $('<td></td>').text('---');
+
                     $tr.append($quantity);
                     $tr.append($name);
                     $tr.append($price);
                     $tr.append($numListed);
                     $tbody.append($tr);
+                }
+                else {
+                    $('<td></td>').text($.get(`https://api.arsha.io/v1/${region}/item?id=${allRecipies[i].components[j].id}`, (data) => {
+                        let $tr = $('<tr></tr>');
+                        let quantity = allRecipies[i].components[j].quantity;
+                        let $quantity = $('<td></td>').text(quantity);
+                        let id = allRecipies[i].id;
+                        for (let k = 0; k < allItems.length; k++) {
+                            if (allItems[k].id == allRecipies[i].components[j].id) {
+                                let name = allItems[k].name;
+                                let price = (data.resultMsg).split('-')[8];
+                                let numListed = (data.resultMsg).split('-')[4];
+
+                                $name = $('<td></td>').text(name).addClass('link');
+                                $name.click(function () { showIngredients(name) });
+                                $price = $('<td></td>').text(price);
+                                $numListed = $('<td></td>').text(numListed);
+
+                                if (!isNaN(price) && price != undefined) {
+                                    console.log('Total 1: ', $('.total.' + id).text());
+                                    $('.total.' + id).text(Number($('.total.' + id).text()) + Number(price) * Number(quantity));
+                                    console.log('Total 2: ', $('.total.' + id).text());
+                                }
+                                break;
+                            }
+                        }
+                        $tr.append($quantity);
+                        $tr.append($name);
+                        $tr.append($price);
+                        $tr.append($numListed);
+                        $tbody.append($tr);
+                    }));
+                }
+            }
+            $table.append($tbody);
+            $container.append($table);
+            $ingredients.append($container);
+            $totalDivs = $('<div></div>').addClass('totalDivs')
+            $totalLabel = $('<div></div>').addClass('total').text('Total Cost: ');
+            $totalDiv = $('<div></div>').addClass('total ' + id).text('0');
+            $totalDivs.append($totalLabel);
+            $totalDivs.append($totalDiv);
+            $container.append($totalDivs);
+            cantFindIt = false;
+            break;
+        }
+    }
+    if (cantFindIt) {
+        let $container = $('<div></div>').addClass('ingredient-list');
+
+        let $header = $('<div></div>').addClass('tableHeader');
+        $header.text(`${itemName}`);
+
+        $errorText = $('<div></div>').addClass('warning').text('No Data Found');
+
+        $container.append($header);
+        $container.append($errorText);
+        $ingredients.append($container);
+    }
+}
+
+function showMaterialGroup(materialGroupNumber) {
+    console.log('Getting Material Group List: ', materialGroupNumber);
+    for (let i = 0; i < allMaterialGroups.length; i++) {
+        if (allMaterialGroups[i].id == materialGroupNumber) {
+            console.log(allMaterialGroups[i]);
+            let $container = $('<div></div>').addClass('materialGroup-list');
+            let $header = $('<div></div>').addClass('tableHeader');
+            $header.text(`Material Group: ${materialGroupNumber}`);
+            $container.append($header);
+
+            let $table = $('<table></table>').addClass('table');
+
+            let headers = ['Name', 'Price', '# Listed'];
+            let $thead = $('<thead></thead>').addClass('thead')
+            let $trhead = $('<tr></tr>');
+            for (let i = 0; i < headers.length; i++) {
+                let $th = $('<th></th>').text(headers[i]);
+                $trhead.append($th);
+            }
+            $thead.append($trhead);
+            $table.append($thead);
+
+            let $tbody = $('<tbody></tbody>').addClass('tbody');
+            let keys = Object.keys(allMaterialGroups[i].mg);
+
+            for (let j = 0; j < keys.length; j++) {
+                console.log('Getting Ingredient: ', keys[j]);
+
+                $('<td></td>').text($.get(`https://api.arsha.io/v1/${region}/item?id=${allMaterialGroups[i].mg[keys[j]].id}`, (data) => {
+                    let $name;
+                    let $price;
+                    let $numListed;
+                    let $tr = $('<tr></tr>');
+                    for (let k = 0; k < allItems.length; k++) {
+                        if (allItems[k].id == allMaterialGroups[i].mg[keys[j]].id) {
+                            $name = $('<td></td>').text(allMaterialGroups[i].mg[keys[j]].name);
+                            $price = $('<td></td>').text((data.resultMsg).split('-')[8]);
+                            $numListed = $('<td></td>').text((data.resultMsg).split('-')[4]);
+                            $tr.append($name);
+                            $tr.append($price);
+                            $tr.append($numListed);
+                            $tbody.append($tr);
+                            break;
+                        }
+                    }
                 }));
             }
             $table.append($tbody);
             $container.append($table);
-            let $pageElement = $('.ingredients');
-            $pageElement.append($container);
+            $ingredients.append($container);
             break;
         }
     }
 }
-
-
-// NOTE: Still need to work on when...
-//     components.id does not exist but
-//     components.materialGroup does exist
-//
-//     and CSS
